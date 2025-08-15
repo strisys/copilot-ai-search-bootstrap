88497e315d7d8bfd77d43811b7bd11a8b7f5771e
Stephen Trudel <github@strisys.com>
2025-08-15 16:56:11 -0400

    fix vars

--- src/main.tf
+++ src/main.tf
@@ -1,6 +1,6 @@
 locals {
-  organization                     = "strisys"
-  project_tag                      = "aisearch"
+  organization                     = var.organization
+  project_tag                      = var.project_tag
   azure_location                   = "eastus"
   azure_resource_group_name        = "rg-${local.organization}-${local.project_tag}"
   azure_resource_group_name_shared = "rg-shared"
@@ -8,7 +8,6 @@ locals {
   time_tag                         = formatdate("HHmmss", timestamp())
   datetime_tag                     = formatdate("YYYYMMDD-HHmmss", timestamp())
   current_date_est                 = formatdate("YYYYMMDD", timeadd(timestamp(), "-5h"))
-  azure_web_app_ip                 = "38.140.3.2"
 
   container_names = [
     "hoisington",
@@ -18,12 +17,6 @@ locals {
   openai_account_name     = "${local.organization}-${local.project_tag}-aisearch"
   openai_embedding_model  = "text-embedding-ada-002"
   openai_embedding_deploy = "embeddings"
-
-  search_service_name = "${local.organization}-${local.project_tag}-aisearch"
-  #   search_data_source_name = "ds-blob-docs"
-  #   search_indexer_name     = "idx-blob-docs"
-
-  search_api_version = "2024-07-01" # Microsoft.Search (indexes/indexers/dataSources/skillsets)
-  openai_api_version = "2024-06-01"
+  search_service_name     = "${local.organization}-${local.project_tag}-aisearch"
 }
 
--- src/variables.tf
+++ src/variables.tf
@@ -4,22 +4,22 @@ variable "environment" {
   default     = "development"
 }
 
+variable "organization" {
+  description = "Organization tag"
+  type        = string
+}
+
+variable "project_tag" {
+  description = "Project tag"
+  type        = string
+}
+
 variable "azure_subscription_id" {
   description = "Azure subscription ID"
   type        = string
 }
 
 variable "azure_tenant_id" {
-  description = ""
-  type        = string
-}
-
-variable "azure_client_id" {
-  description = ""
-  type        = string
-}
-
-variable "azure_client_secret" {
   description = ""
   type        = string
 }
